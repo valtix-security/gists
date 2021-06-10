17b85e06bf69b415072da1d3ccd6766f931309d7
gqloungin <stagefivegsr@hotmail.com>
2021-06-10 16:12:51 -0700

    added confirmation and modified to cmdlets

--- valtix_azure_roles/valtix-controller-role.ps1
+++ valtix_azure_roles/valtix-controller-role.ps1
@@ -8,60 +8,93 @@
 # - Open Azure Cloud Shell in the subscription you wish to register as account in Valtix
 # - run script ./valtix-controller-role.ps1
 #
-# Begin Script
+# Parameters
+$AppName = "ValtixApp"
+$CustomRoleName = "valtix-controller-role"
+$SecretKeyName = "client-secret"
 
+# Begin Script
 Write-Host "Setting up Valtix Controller Application registration & Service Principal.."
 
-$AppID=az ad app create --display-name 'ValtixApp' --available-to-other-tenants true --query 'appId' -o tsv
-$SPID=az ad sp create --id $AppID --query 'objectId' --output tsv
-$Secret=az ad app credential reset --id $AppID --append --credential-description 'client-secret' --query 'password' -o tsv
-
 # Get Subscription & Directory (Tenant) IDs
-$SubscriptionID=(Get-AzContext).Subscription.id
-$DirectoryID=(Get-AzContext).Tenant.Id
+$SubscriptionObj=(Get-AzContext).Subscription
+$DirectoryObj=(Get-AzContext).Tenant
 
+# Check to see if this is the correct subscription
+Write-Host ""
+Write-Host "-----------------------------------------------------------------"
+Write-Host "Subscription Name:" $SubscriptionObj.Name
+Write-Host "Subscription ID:" $SubscriptionObj.id
+Write-Host "Directory ID:" $DirectoryObj.id
+Write-Host "-----------------------------------------------------------------"
+Write-Host ""
+$confirmation = Read-Host "Valtix AzureAD App Registration will be made to this Directory and Subscription.  Would you like to continue? [y/n]"
+while($confirmation -ne "y")
+{
+    if ($confirmation -eq 'n') 
+    {
+        Write-Host ""
+        Write-Host "If this is the incorrect Subscription, use the following cmdlet to change it in Azure Cloud Shell:"
+        Write-Host "Set-AzContext -Tenant <tenant id>"
+        Write-Host ""
+        exit
+    }
+    $confirmation = Read-Host "Ready? [y/n]"
+}
+
+# this is a workaround for Connect-AzureAD not working in PS1
+# more info here: https://github.com/Azure/CloudShell/issues/72
+import-module AzureAD.Standard.Preview
+AzureAD.Standard.Preview\Connect-AzureAD -Identity -TenantID $DirectoryObj.id|Out-Null        
+
+$AppObj=New-AzureADApplication -DisplayName $AppName -AvailableToOtherTenants 1
+$SPObj=New-AzADServicePrincipal -DisplayName ServicePrincipalName -ApplicationId $AppObj.AppId -SkipAssignment
+$SecretObj=New-AzureADApplicationPasswordCredential -ObjectId $AppObj.ObjectId -CustomKeyIdentifier $SecretKeyName
+
+Write-Host ""
 Write-Host "Creating custom role definition.."
 $role = Get-AzRoleDefinition "Virtual Machine Contributor"
 $role.Id = $null
-$role.Name = "valtix-controller-role"
+$role.Name = $CustomRoleName
 $role.Description = "this role allows valtix-controller to deploy resources into the subscription"
 $role.Actions.Clear()
-$role.Actions.Add("Microsoft.Compute/virtualMachines/*")
+$role.Actions.Add("Microsoft.ApiManagement/service/*")
 $role.Actions.Add("Microsoft.Compute/disks/*")
+$role.Actions.Add("Microsoft.Compute/images/read")
+$role.Actions.Add("Microsoft.Compute/sshPublicKeys/read")
+$role.Actions.Add("Microsoft.Compute/virtualMachines/*")
+$role.Actions.Add("Microsoft.ManagedIdentity/userAssignedIdentities/read")
+$role.Actions.Add("Microsoft.ManagedIdentity/userAssignedIdentities/assign/action")
 $role.Actions.Add("Microsoft.Network/loadBalancers/*")
+$role.Actions.Add("Microsoft.Network/networkinterfaces/*")
+$role.Actions.Add("Microsoft.Network/networkSecurityGroups/*")
 $role.Actions.Add("Microsoft.Network/publicIPAddresses/*")
+$role.Actions.Add("Microsoft.Network/routeTables/*")
 $role.Actions.Add("Microsoft.Network/virtualNetworks/*")
 $role.Actions.Add("Microsoft.Network/virtualNetworks/subnets/*")
 $role.Actions.Add("Microsoft.Resources/subscriptions/resourcegroups/*")
-$role.Actions.Add("Microsoft.Network/networkSecurityGroups/*")
-$role.Actions.Add("Microsoft.Network/networkinterfaces/*")
-$role.Actions.Add("Microsoft.Network/routeTables/*")
 $role.Actions.Add("Microsoft.Storage/storageAccounts/blobServices/*")
-$role.Actions.Add("Microsoft.Compute/images/read")
-$role.Actions.Add("Microsoft.ApiManagement/service/*")
-$role.Actions.Add("Microsoft.Storage/storageAccounts/blobServices/*")
-$role.Actions.Add("Microsoft.ManagedIdentity/userAssignedIdentities/read")
 $role.AssignableScopes.Clear()
-$role.AssignableScopes.Add("/subscriptions/$SubscriptionID")
+$role.AssignableScopes.Add("/subscriptions/" + $SubscriptionObj.id)
 
 # create the custom role
 Write-Host "Creating custom role in IAM.."
-$RoleName=New-AzRoleDefinition -Role $role | select -expand Name
+$CustomRoleObj=New-AzRoleDefinition -Role $role
 
 # role assignment to app
 Write-Host "Assigning custom role to App Service Principal.."
-New-AzRoleAssignment -ObjectId $SPID -RoleDefinitionName $RoleName | Out-Null
+New-AzRoleAssignment -ObjectId $SPObj.Id -RoleDefinitionName $CustomRoleObj.Name | Out-Null
 
 #Accept Marketplace Terms
 Write-Host "Accepting Marketplace Terms.."
-az vm image terms accept --publisher valtix --offer datapath --plan valtix_dp_image --subscription $SubscriptionID -o none
+az vm image terms accept --publisher valtix --offer datapath --plan valtix_dp_image --subscription $SubscriptionObj.id -o none
 
 # Output
 Write-Host "-----------------------------------------------------------------"
 Write-Host "Completed role setup"
-Write-Host "Directory ID:" $DirectoryID
-Write-Host "Subscription ID:" $SubscriptionID
-Write-Host "Application ID:" $AppID
-Write-Host "Client Secret:" $Secret
+Write-Host "Directory ID:" $DirectoryObj.id
+Write-Host "Subscription ID:" $SubscriptionObj.id
+Write-Host "Application ID:" $AppObj.AppId
+Write-Host "Client Secret:" $SecretObj.value
 Write-Host "-----------------------------------------------------------------"
 
