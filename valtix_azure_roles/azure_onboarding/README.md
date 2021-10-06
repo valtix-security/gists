fe7d73f6feb33df2a1a37c3af39febc00eb363aa
Ubuntu <ubuntu@ip-172-31-81-94.ec2.internal>
2021-10-06 05:37:52 +0000

    Adding initial script

---
+++ valtix_azure_roles/azure_onboarding/README.md
@@ -0,0 +1,1 @@
+Currenting under development.
---
+++ valtix_azure_roles/azure_onboarding/valtix-onboarding.ps1
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/valtix-security/gists/main/valtix_azure_roles/valtix-controller-role.ps1
+
+echo "Getting the onboarding file"
