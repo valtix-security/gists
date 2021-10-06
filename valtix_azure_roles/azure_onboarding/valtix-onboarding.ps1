225e704bd4cff0faea56ada54d7244844cb07633
Ubuntu <ubuntu@ip-172-31-81-94.ec2.internal>
2021-10-06 19:02:36 +0000

    test

--- valtix_azure_roles/azure_onboarding/valtix-onboarding.ps1
+++ valtix_azure_roles/azure_onboarding/valtix-onboarding.ps1
@@ -1,3 +1,7 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/valtix-security/gists/main/valtix_azure_roles/valtix-controller-role.ps1 -OutFile test-onboarding.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/valtix-security/gists/main/valtix_azure_roles/role.json -OutFile role.json
+
+Get-Location
+ls
 
 echo "Getting the onboarding file"
