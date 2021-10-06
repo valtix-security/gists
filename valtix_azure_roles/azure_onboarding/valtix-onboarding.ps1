a275add248c6e520128c9aaa800e23cb6638ed3e
Ubuntu <ubuntu@ip-172-31-81-94.ec2.internal>
2021-10-06 19:23:09 +0000

    test

--- valtix_azure_roles/azure_onboarding/valtix-onboarding.ps1
+++ valtix_azure_roles/azure_onboarding/valtix-onboarding.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/valtix-security/gists/main/valtix_azure_roles/valtix-controller-role.ps1 -OutFile test-onboarding.ps1
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/valtix-security/gists/main/valtix_azure_roles/role.json -OutFile role.json
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/valtix-security/gists/main/valtix_azure_roles/azure_onboarding/role.json -OutFile role.json
 
 output = Get-Location
 output1 = ls
