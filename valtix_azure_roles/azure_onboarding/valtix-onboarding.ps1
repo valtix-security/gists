04f84f55f8e7ae148c7da5e0cdfc7fccd60170ae
Ubuntu <ubuntu@ip-172-31-81-94.ec2.internal>
2021-10-07 17:47:03 +0000

    test

--- valtix_azure_roles/azure_onboarding/valtix-onboarding.ps1
+++ valtix_azure_roles/azure_onboarding/valtix-onboarding.ps1
@@ -1,7 +1,7 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/valtix-security/gists/main/valtix_azure_roles/azure_onboarding/role.json -OutFile role.json
 
-APP_NAME=michael-valtix-controller-app
-ROLE_NAME=michael-valtix-controller-role
+APP_NAME="michael-valtix-controller-app"
+ROLE_NAME="michael-valtix-controller-role"
 
 account_info=$(az account show)
 sub_id=$(echo $account_info | jq -r .id)
